ea0e9d031f2b592ffa80c8830117907defcc401f
nothrishit <hrishitjohn@gmail.com>
2023-04-05 10:42:42 +0530

    added sign in and sign up controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,15 @@ module.exports.profile = function(req, res){
         title : 'User'
     });
 }
+//rendering sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    });
+}
+//rendering sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    });
+}
