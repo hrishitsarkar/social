0ef7810d42d4b054a9bd21e601a1b0e5bb2943d6
nothrishit <hrishitjohn@gmail.com>
2023-04-10 13:27:14 +0530

    added and displayed posts and authentication:done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,22 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 66);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 66);
+
+    
+//populating user
+    Post.find({}).populate('user').exec().then((posts)=>{
+        
+            
+            return res.render('home', {
+                title: "Home",
+                posts:posts
+            });
+        
+    }).catch((err)=>{
+        console.log(`err : ${err}`);
     });
+    
 }
 
 // module.exports.actionName = function(req, res){}
